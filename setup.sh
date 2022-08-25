b5c89dc4818b7d0d76d443ca1389a9ba27d032f8
Guilherme Filippo <guilherme.filippo@icloud.com>
2022-08-25 18:02:51 -0300

    New folders to migrate.

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 apt update &>/dev/null && apt dist-upgrade -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update &>/dev/null && apt install docker-ce -y
+apt update &>/dev/null && apt install docker-ce nfs-common -y
+mkdir -p /mg/mx && mount 10.100.1.34:/mg/mx /mg/mx
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod 700 /usr/local/bin/docker-compose
 systemctl disable systemd-resolved
