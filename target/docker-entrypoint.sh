793505ae070f15a9ba47252fa8737f5056a63f52
Guilherme Filippo <guilherme.filippo@icloud.com>
2022-08-25 23:08:27 -0300

    Atualizando locais.

--- target/docker-entrypoint.sh
+++ target/docker-entrypoint.sh
@@ -58,26 +58,31 @@ function setup_environment
         touch $ZIMBRA_ENVIRONMENT_PATH/.dont_start_zimbra
 
         echo "Copying customized scripts"
+        mkdir -p $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts
+        cp /app/setup-cbpolicyd.sh $ZIMBRA_ENVIRONMENT_PATH/app/
+        
         cp /app/queue-control.sh $ZIMBRA_ENVIRONMENT_PATH/root/
         cp /app/queue-mon.sh $ZIMBRA_ENVIRONMENT_PATH/root/
         cp /app/certbot-zimbra.sh $ZIMBRA_ENVIRONMENT_PATH/root/
-        cp /app/zcs-export.sh $ZIMBRA_ENVIRONMENT_PATH/root/
-        cp /app/zcs-import.sh $ZIMBRA_ENVIRONMENT_PATH/root/
-        cp /app/enable-antispam.sh $ZIMBRA_ENVIRONMENT_PATH/root/
-        cp /app/change-date-pop.sh $ZIMBRA_ENVIRONMENT_PATH/root/
         cp /app/queue-delete.pl $ZIMBRA_ENVIRONMENT_PATH/root/
-	    cp /app/setup-cbpolicyd.sh $ZIMBRA_ENVIRONMENT_PATH/app/
+
+        cp /app/zcs-export.sh $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/
+        cp /app/zcs-import.sh $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/
+        cp /app/enable-antispam.sh $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/
+        cp /app/change-date-pop.sh $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/
 
         echo "Setting customized permissions"
+        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/app/setup-cbpolicyd.sh
+        
         chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/queue-control.sh
         chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/queue-mon.sh
         chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/certbot-zimbra.sh
-        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/zcs-export.sh
-        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/zcs-import.sh
-        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/enable-antispam.sh
-        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/change-date-pop.sh
         chmod 750 $ZIMBRA_ENVIRONMENT_PATH/root/queue-delete.pl
-	    chmod 750 $ZIMBRA_ENVIRONMENT_PATH/app/setup-cbpolicyd.sh
+	    
+        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/zcs-export.sh
+        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/zcs-import.sh
+        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/enable-antispam.sh
+        chmod 750 $ZIMBRA_ENVIRONMENT_PATH/opt/zimbra/scripts/change-date-pop.sh
 
         echo "Installing crontab"
         #echo "*/1 * * * * /root/queue-control.sh" >> /etc/crontab
