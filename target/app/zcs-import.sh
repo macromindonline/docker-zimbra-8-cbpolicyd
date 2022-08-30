e493094433f9b6a61b053ed559b465aef4c08e2c
Guilherme Filippo <guilherme.filippo@icloud.com>
2022-08-30 00:56:33 -0300

    Atualizando

--- target/app/zcs-import.sh
+++ target/app/zcs-import.sh
@@ -28,6 +28,8 @@ else
             zmprov ca ${ACCOUNT_NAME} ChangeMe@123
             echo "Configuring Pop3 download date to ${ACCOUNT_NAME}"
             zmprov ma ${ACCOUNT_NAME} zimbraPrefPop3DownloadSince $(date "+%Y%m%d%H%M%S"Z)
+            echo "Force ${ACCOUNT_NAME} to change password at next logon"
+            zmprov ma ${ACCOUNT_NAME} zimbraPasswordMustChange TRUE
             echo "${ACCOUNT_NAME} => DisableWarnings"
             zmmailbox -z -m ${ACCOUNT_NAME} addFilterRule "DisableWarnings" active any address "from" all contains "MAILER-DAEMON" discard
             echo "${ACCOUNT_NAME} => AntispamTitle"
